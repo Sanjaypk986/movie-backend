33cb6f8ae7963f7872bbeaac11d5c20bb7c764df
SANJAY <sanjaypk986@gmail.com>
2024-07-02 11:36:49 +0530

    review filtering with query parameter

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,19 +2,32 @@ const Review = require("../models/reviewModel");
 
 // get all review
 const getAllReview = async (req, res) => {
-  const reviews = await Review.find({});
-  res.json(reviews);
+  try {
+    const reviews = await Review.find(req.query);
+    res.json(reviews);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 };
 // add Review
 const addReview = async (req, res) => {
-    // create document using req.body
+  // create document using req.body
+  try {
     const review = new Review({
       ...req.body,
-      user: req.user._id
+      user: req.user._id,
     });
     // save document
     await review.save();
     res.send(review);
-  };
+  } catch (error) {
+    res.status(400).send("Please check data");
+  }
+};
 
-  module.exports = {addReview,getAllReview}
+module.exports = { addReview, getAllReview };
+
+
+// 666e9c65d57ea4c2b19e7333
+// 665f49403af56756806dbe4b
+// 666e9c65d57ea4c2b19e7333
